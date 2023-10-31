bf187446082592dc72ba58c4a4c6b7beb118ff5d
ms3 <ms3.msssivadas@gmail.com>
2023-10-31 17:45:04 +0530

    updated profiles.py

--- cic/models/halos/density/profiles.py
+++ cic/models/halos/density/profiles.py
@@ -18,22 +18,17 @@ class NFW(DensityProfile):
         return np.log( np.add( 1, c ) ) - np.divide( c, np.add( 1, c ) )
     
     def f(self, x: Any, c: Any) -> Any: 
-
         cx  = np.multiply(c, x)
         res =  ( cx * (1 + cx)**2 )**-1
         return res
     
     def u(self, q: Any, c: Any) -> Any:
-        
         qc, cp1  = np.divide(q, c), np.add(1, c)
-
         res      = cp1 * qc
         si1, ci1 = sici( qc )
         si2, ci2 = sici( res )
-
-        res = (si2 - si1) * np.sin( qc ) + (ci2 - ci1) * np.cos( qc ) - np.sin(q) / res
-        res = res / np.power(c, 3.)
-        return res
+        res      = (si2 - si1) * np.sin( qc ) + (ci2 - ci1) * np.cos( qc ) - np.sin(q) / res
+        return res / np.power(c, 3.)
            
 nfw = NFW( bullock01 )
 builtinProfiles['nfw'] = nfw
