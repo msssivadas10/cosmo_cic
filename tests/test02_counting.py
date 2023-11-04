144533d34104d0edf7734a2a6af90c7e68d8bb22
ms3 <ms3.msssivadas@gmail.com>
2023-11-04 22:52:44 +0530

    updated test

--- tests/test02_counting.py
+++ tests/test02_counting.py
@@ -7,24 +7,62 @@ sys.path.append(path.split(path.split(__file__)[0])[0])
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import gammaln, erf
-from cic.measure2.utils import Rectangle
-from cic.measure2.counting import prepareRegion, estimateCellQuality, estimateObjectCount
-from cic.measure2.utils import CountResult
-from cic.measure2.stats import jackknife
 
 
 # setup logging
 logging.basicConfig(level    = logging.INFO,
                     format   = "%(asctime)s [%(levelname)s] %(message)s",
                     handlers = [ logging.StreamHandler() ])
-# logging.disable()
+logging.disable()
 
 
 datafile = 'p3catalog.csv'
-outfile  = 'test_counts.json'
+outfile  = 'test_counts1.json'
+
+def doCounting_old():
+
+    from cic.measure.utils import Rectangle
+    from cic.measure.counting import prepareRegion, countObjects
+    from cic.measure.utils import CountResult
+    from cic.measure.stats import jackknife
+
+    prepareRegion(path = datafile,
+                  output_path = outfile,
+                  region = Rectangle(0., 0., 40., 10.),
+                  patchsize_x = 10.,
+                  patchsize_y = 10.,
+                  pixsize = 1.,
+                  bad_regions = [],
+                  use_masks = ['g_mask', 'r_mask', 'i_mask'],
+                  data_filters = [],
+                  expressions = [],
+                  x_coord = 'ra',
+                  y_coord = 'dec',
+                  chunksize = 10000,
+                  header = 0,)
+    
+    countObjects(path = datafile, 
+                 patch_details_path = outfile, 
+                 output_path = outfile, 
+                 include_patch_details = 1,
+                 use_masks =  ['g_mask', 'r_mask', 'i_mask'], 
+                 data_filters = ['g_magnitude < 22.0'], 
+                 expressions = [], 
+                 x_coord = 'ra', 
+                 y_coord= 'dec', 
+                 chunksize = 10000,
+                 header = 0,)
+    
+    return
 
 def doCounting():
+
+    from cic.measure2.utils import Rectangle
+    from cic.measure2.counting import prepareRegion, estimateCellQuality, estimateObjectCount
+
+
+    csv_opts = dict(chunksize = 10000,
+                    header    = 0,   )
 
     # prepare for counting
     prepareRegion(output_path = outfile,
@@ -41,8 +79,7 @@ def doCounting():
                         data_filters       = [],
                         expressions        = [],
                         coord              = ['ra', 'dec'],
-                        chunksize          = 10000,
-                        header             = 0,   )
+                        csv_opts           = csv_opts,    )
 
     # estimate object counts
     estimateObjectCount(path               = datafile, 
@@ -52,13 +89,15 @@ def doCounting():
                         data_filters       = ['g_magnitude < 22.0'], 
                         expressions        = [], 
                         coord              = ['ra', 'dec'], 
-                        chunksize          = 10000,
-                        header             = 0,   )
+                        csv_opts           = csv_opts,    )
     
     return
 
-
 def checkResult():
+
+    from scipy.special import gammaln, erf
+    from cic.measure2.utils import CountResult
+    from cic.measure2.stats import jackknife
 
     @jackknife
     def mean(x): return np.mean(x) # mean with jacknife resampling
@@ -71,20 +110,25 @@ def checkResult():
     # load saved result
     res = CountResult.load( outfile )
 
+    res.add( np.cumsum( res.get('total_count'   ), axis = -2 ), 'total_count'   , replace = True )
+    res.add( np.cumsum( res.get('unmasked_count'), axis = -2 ), 'unmasked_count', replace = True )
+
     # estimated distribution
     bins       = np.arange(195.5, 350.5, 5.0)
-    hist       = res.histogram('total_count', bins = bins, density = True).hist
+    # bins, hist = res.histogram('total_count', bins = bins, density = True)#.hist
+    hist = []
+    for i in range(res.shape[-1]):
+        hist.append( np.histogram(res.get('total_count')[:,:,-1,i].flatten(), bins, density = 1)[0] )
+    hist = np.stack(hist, -1)
     meanResult = mean(hist)
-    # print(hist)
 
     x    = 0.5 * ( bins[1:] + bins[:-1] )
     yEst = meanResult.estimate
     yErr = meanResult.error
 
-
     # expected distribution: poisson process with normal distributed feature 'g_magnitude'
-    density = 208979 / 400   # density = catalog size / area. original density = 500.
-    area    = res.pixsize**2 # cell area
+    density = 208979 / 400         # density = catalog size / area. original density = 500.
+    area    = np.prod(res.pixsize) # cell area
     lam     = density * area * normalCDF(22., loc = 22., scale = 5)
     yExp    = poissonPMF(x, lam = lam)
 
@@ -96,5 +140,5 @@ def checkResult():
     return
 
 
-doCounting()
+# doCounting()
 checkResult()
