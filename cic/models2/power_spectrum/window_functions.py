bc0b153c2b6df47c7f7eb1a1d1535b1e8bf30733
ms3 <ms3.msssivadas@gmail.com>
2023-12-29 01:21:54 +0530

    expressed in terms of hypergeometric

--- cic/models2/power_spectrum/window_functions.py
+++ cic/models2/power_spectrum/window_functions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import numpy as np
+from scipy.special import eval_hermite, hyp0f1
 from typing import Any 
 from .._base import WindowFunction
 
@@ -8,56 +9,37 @@ class SphericalTophat(WindowFunction):
     r"""
     Spherical top-hat filter.
     """
-    def __init__(self, disable_masks: bool = False) -> None:
-        super().__init__()
-        # mask at zero and inifinity ends to make the function smooth
-        self.disableMasks = disable_masks 
-
     def call(self, x: Any, deriv: int = 0) -> Any:
-        # sherical top-hat function
-        def f1(x): return 3.*( np.sin(x) - x * np.cos(x) ) / x**3
-        # first derivative 
-        def f2(x): return 3.*( ( x**2 - 3. ) * np.sin(x)  + 3.*x * np.cos(x) ) / x**4
-        # second derivative
-        def f3(x): return 3.*( ( x**2 - 12. ) * x * np.cos(x)  - ( 5*x**2 - 12. ) * np.sin(x) ) / x**5
+        # tophat function in terms of hypergeometric function 0f1 (to avoid zero division)
+        def _g(x: Any, nu: int = 0, a: float = 2.5) -> Any:
+            # top-hat function: w(x) = 3*( sin(x) - x * cos(x) ) / x**3
+            if nu == 0: return hyp0f1(a, -0.25*x**2)
+            # first derivative
+            if nu == 1: return -x * hyp0f1(a + 1, -0.25*x**2) / (2*a)
+            # second derivative
+            if nu == 2: return (2*a - 1) / (2*a) * hyp0f1(a + 1, -0.25*x**2) - hyp0f1(a, -0.25*x**2)
+            # general derivative. TODO: check this relation
+            return (2*a - 1) / (2*a) * _g(x, nu - 2, a + 1) - _g(x, nu - 2, a)	
         
-        x = np.asfarray( x )
-        if self.disableMasks:
-            if deriv == 0: return f1(x)
-            if deriv == 1: return f2(x)
-            if deriv == 2: return f3(x)
-        else:
-            # mask for x closer to 0
-            closeToZero = ( np.abs(x) < 1e-03 )
-            # mask for other smallr x; at both ends, the function values is almost 0
-            otherValues = np.logical_not( closeToZero | ( np.abs(x) > 1e+04 ) )
-            res = np.zeros_like( x )
-            if deriv == 0:
-                res[closeToZero] = 1.0
-                res[otherValues] = f1( x[otherValues] )
-                return res
-            if deriv == 1:
-                res[otherValues] = f2( x[otherValues] )
-                return res
-            if deriv == 2:
-                res[closeToZero] = -0.2
-                res[otherValues] = f3( x[otherValues] )
-                return res
-        raise ValueError(f"invalid value for argument 'deriv': {deriv}")
-    
+        if deriv < 0:
+            raise ValueError(f"invalid value for argument 'deriv': {deriv}")
+        res = _g(np.asfarray(x), deriv, 2.5)
+        return res
+ 
 class Gaussian(WindowFunction):
     r"""
     Gaussian function.
     """
     def call(self, x: Any, deriv: int = 0) -> Any:
-        x = np.asfarray( x )
-        if deriv == 0:
-            return np.exp( -0.5*x**2 )
-        if deriv == 1:
-            return -x * np.exp( -0.5*x**2 )
-        if deriv == 2:
-            return ( x**2 - 1 ) * np.exp( -0.5*x**2 )
-        raise ValueError(f"invalid value for argument 'deriv': {deriv}")
+        SQRT_2 = 1.4142135623730951
+        if deriv < 0:
+            raise ValueError(f"invalid value for argument 'deriv': {deriv}")
+        x   = np.divide(x, SQRT_2)
+        res = np.exp(-x**2)
+        if deriv > 0:
+            # derivatives in terms of hermite polynomials
+            res = res * eval_hermite(deriv, x) / (-SQRT_2)**deriv
+        return res
 
 
 # initialising models to be readily used
