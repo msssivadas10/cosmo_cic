5a44bbc9a53cf677b8a0a7f06f3d97c3c3e9c6f5
ms3 <ms3.msssivadas@gmail.com>
2024-01-24 01:38:09 +0530

    test

--- tests/test00_gen.py
+++ tests/test00_gen.py
@@ -17,18 +17,22 @@ cm.link(power_spectrum = 'eisenstein98_zb',
         halo_bias      = 'tinker10', 
         cmreln         = 'bullock01_powerlaw',
         halo_profile   = 'nfw',   )
+# print( cm )
 
 Muv  = -20.5
 zbar = 1.7
-hm = Zheng07.predefined('harikane22', Muv, zbar, cm, 200.)
+hm = Zheng07.harikane22(Muv, zbar, cm, 200.)
+print( hm )
 
 print( ( hm.effectiveBias( zbar ) ) )
+x = 12.72
+# print( 3.16 * x - 24.33 )
 
 # plt.figure()
-# r = np.logspace(-3, 3, 11)
-# y  = cm.matterCorrelation(r, 0, 1)
-# y1 = cm.matterCorrelation(r, 0, 1, ht = 1)
+# x = np.logspace(6, 18, 11)
 # plt.loglog()
-# plt.plot(r, y, '-s')
-# plt.plot(r, y1, '-o')
+# y = cm.haloMassFunction(x, zbar, 200.)
+# plt.plot(x, y, '-')
+# y = hm.massFunction(x, zbar)
+# plt.plot(x, y, 's')
 # plt.show()
