23024a2a954aa6442c048d2ba1446d89c432e490
ms3 <ms3.msssivadas@gmail.com>
2024-03-21 10:48:25 +0530

    test

--- tests/test00_gen.py
+++ tests/test00_gen.py
@@ -6,8 +6,28 @@ sys.path.append(path.split(path.split(__file__)[0])[0])
 
 import numpy as np
 import matplotlib.pyplot as plt
-from cic.models2 import cosmology, Cosmology
+from scipy.interpolate import CubicSpline
+from cic.models2 import cosmology, Cosmology, PowerSpectrum
 from cic.models2.stats.hod import Zheng07
+from typing import Any 
+
+class SampledPS(PowerSpectrum):
+    __slots__ = 'lnk', 'lnt', 'interp'
+    def __init__(self, lnk: Any, lnt: Any) -> None:
+        super().__init__()
+        self.lnk, self.lnt = lnk, lnt # lnp = 3*lnk + 2*lnt
+        self.interp = CubicSpline(lnk, lnt)
+
+    def call(self, model: Cosmology, k: Any, z: Any, deriv: int = 0, **kwargs: Any) -> Any:
+        res = np.exp( self.interp( np.log(k), nu = 0 ) )
+        # interpolation with linear growth factor (normalised using value at z = 0)
+        if deriv:
+            res = res * self.interp( np.log(k), nu = 1 ) / np.asfarray(k)
+            res = res + np.zeros_like( z )
+        else:
+            res = res * model.dplus( z ) / model.dplus( 0. )
+        return res
+    
 
 cm = cosmology('plank18')
 cm.link(power_spectrum = 'eisenstein98_zb', 
@@ -17,6 +37,8 @@ cm.link(power_spectrum = 'eisenstein98_zb',
         cmreln         = 'bullock01_powerlaw',
         halo_profile   = 'nfw',   )
 # print( cm )
+
+############################################################################################
 
 def test2():
     MUV_TH = -22.5 # uv magnitude threshold
@@ -215,23 +237,139 @@ def test3():
     ylab  = '$n_g$'
 
     plt.figure()
-    plt.loglog()
-    # plt.plot(z, np.abs(y - y_ref)/np.abs(y_ref), '-', label = 'pyccl')
-    plt.plot(z, y_ref, '-', label = 'pyccl')
-    plt.plot(z, y, '-', label = 'ente')
+    # plt.loglog()
+    # plt.plot(z, 100*np.abs(y - y_ref)/np.abs(y_ref), '-', label = 'pyccl')
+    plt.plot(z, y / y_ref, '-', label = 'pyccl')
+    # plt.plot(z, y_ref, '-', label = 'pyccl')
+    # plt.plot(z, y, '-', label = 'ente')
     plt.xlabel( 'z' )
     plt.ylabel( ylab )
     plt.title(f'max. error: { max_error(y, y_ref) :.3g} %')
     plt.legend()
     plt.show()
+    return
 
+def test4():
+    from scipy.integrate import simpson
+
+    h = 1. # cm.h
+    hm = Zheng07(logm_min = np.log10(1e11 / h), 
+                 sigma_logm = 0.25, 
+                 logm0 = np.log10(1e8 / h), 
+                 logm1 = np.log10(1e12 / h), 
+                 alpha = 0.8, 
+                 cosmology = cm, 
+                 overdensity = 200, )
+
+    z, ng, mh, bg = np.loadtxt('../ref/ref3/hod_values.txt', delimiter = ',', comments = '#', unpack = True)
+    mf_table = np.loadtxt('../ref/ref3/hod_values_mf.txt', delimiter = ',', comments = '#')
+    bf_table = np.loadtxt('../ref/ref3/hod_values_bf.txt', delimiter = ',', comments = '#')
+
+    plt.figure()
+    # plt.loglog()
+    # plt.semilogy()
+    plt.semilogx()
+
+
+    # for zz in range(0, len(z), 10):
+    #     y = hm.massFunction( mf_table[:,0] * cm.h, z[ zz ] ) #* cm.h
+    #     plt.plot( mf_table[:,0], 100 * np.abs( y / mf_table[:,zz+2] - 1. ), label = '%.2f' % z[ zz ] )
+    #     # plt.plot( mf_table[:,0], mf_table[:,zz+2] )
+    #     # plt.plot( mf_table[:,0], y, '+')
+    # plt.legend(title = 'z')
+
+    for zz in range(0, len(z), 10):
+        y = hm.biasFunction( bf_table[:,0] * cm.h, z[ zz ] ) #* cm.h
+        plt.plot( bf_table[:,0], 100 * np.abs( y / bf_table[:,zz+2] - 1. ), label = '%.2f' % z[ zz ] )
+        # plt.plot( mf_table[:,0], mf_table[:,zz+2] )
+        # plt.plot( mf_table[:,0], y, '+')
+    plt.legend(title = 'z')
+
+
+    x = np.log10( mf_table[:,0] )
+    y = []
+    for zz in range(len(z)):
+        # y = hm.massFunction( mf_table[:,0] * cm.h, z[ zz ] ) #* cm.h**3
+        # plt.plot( mf_table[:,0], mf_table[:,zz+2] )
+        # plt.plot( mf_table[:,0], y)
+        # plt.plot( mf_table[:,0], mf_table[:,zz+2] / y )
+
+        ...
+        # y.append( simpson( mf_table[:,1] * mf_table[:,zz+2], x = x ) )
+        # y.append( simpson( mf_table[:,0] * mf_table[:,1] * mf_table[:,zz+2], x = x ) )
+        # y.append( simpson( bf_table[:,zz+2] * mf_table[:,1] * mf_table[:,zz+2], x = x ) )
+    # y = np.asfarray(y)
+
+    ## total
+    # plt.plot( mf_table[:,0], mf_table[:,1] )
+    # plt.plot( mf_table[:,0], hm.totalCount( mf_table[:,0] ) )
+
+    # plt.plot(z, ng) 
+    # plt.plot(z, y, '+') 
+
+    plt.show()
+
+    return
+
+def test5():
+
+    cm = Cosmology(h = 0.6774, 
+                   Om0 = 0.229 + 0.049, 
+                   Ob0 = 0.049, 
+                   sigma8 = 0.8159, 
+                   ns = 0.9667)
+    lnh = np.log( cm.h )
+
+    x, y = np.loadtxt('../ref/ref2/linear_power_z0.00.txt', delimiter = ',', comments = '#', unpack = True)
+
+    # convert k from mpc^-1 to h/mpc
+    x = np.log(x) - lnh
+
+    # convert p(k) from mpc^-3 to (h/mpc)^3
+    y = np.log(y) - 3*lnh
+
+    # convert ln(p) to ln(t)
+    y = 0.5*( y - cm.ns*x )
+    y = y - np.max(y)
+
+    cm.link(power_spectrum = SampledPS(x, y), 
+            window         = 'tophat', 
+            mass_function  = 'tinker08', 
+            halo_bias      = 'tinker10', 
+            cmreln         = 'bullock01_powerlaw',
+            halo_profile   = 'nfw',   )
+    
+    ##
+    z = 0.
+
+    fig = plt.figure()
+    plt.semilogx()
+    # plt.loglog()
+
+    for z in [ 0., 1., 2., 4., ]:
+        # power
+        k, pk_ref = np.loadtxt('../ref/ref2/linear_power_z%.2f.txt' % z, delimiter = ',', comments = '#', unpack = True)
+        pk = cm.matterPowerSpectrum( k[::20]/cm.h, z ) / cm.h**3
+        plt.plot(k[::20], np.abs( pk / pk_ref[::20] - 1 )*100, '-', label = '%.2f' % z)
+
+        # m, s_ref, dlns_ref, f_ref, hmf_ref, b_ref = np.loadtxt('../ref/ref2/halo_props_z%.2f.txt' % z, delimiter = ',', comments = '#', unpack = True)
+        # # y_ref, y = s_ref**2, cm.matterVariance( cm.lagrangianR( m*cm.h ), z ) # var
+        # # y_ref, y = hmf_ref, cm.haloMassFunction( m*cm.h, z, 200 ) * cm.h**3 # massfunc
+        # y_ref, y =  b_ref, cm.haloBias(m*cm.h, z, 200) # bias
+        # plt.plot(m, np.abs( y / y_ref - 1) * 100, '-', label = '%.2f' % z)
+
+
+    plt.legend(title = 'z')
+    plt.show()
 
     return
 
 def main():
     # test1()
     # test2()
-    test3()
+    # test3()
+    # test4()
+    test5()
     return
 
 if __name__ =='__main__':
